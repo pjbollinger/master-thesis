a78d0885a13e5d8da0acc9f5b1838c8db3536b34
Patrick Bollinger <owner@pjbollinger.com>
2019-04-14 16:30:17 -0400

    Reduce complexity of FullAdder
    Also added pytest.ini to simplify running pytest command.

--- pyfactorization/FullAdder.py
+++ pyfactorization/FullAdder.py
@@ -1,6 +1,7 @@
 from . import gates
 from . import HalfAdder
 
+
 class FullAdder:
     def __init__(self, bits=list("ZZZZZ")):
         self.bits = [bits[0], bits[1], bits[2], 'Z', 'Z', 'Z', bits[3], bits[4]]
@@ -15,68 +16,87 @@ class FullAdder:
         #          └------- 0 input A
 
     def solve(self, change='0'):
-        if self.bits[0] == 'Z' and self.bits[1] == '1' and self.bits[2] == 'Z' and self.bits[6] == '0' and self.bits[7] == 'Z':
-            # Special case 1
+        if (
+            (
+                self.bits[0] == 'Z'
+                and self.bits[1] == '1'
+                and self.bits[2] == 'Z'
+                and self.bits[6] == '0'
+                and self.bits[7] == 'Z'
+            )  # Special case 1
+            or (
+                self.bits[0] == 'Z'
+                and self.bits[1] == '1'
+                and self.bits[2] == '1'
+                and self.bits[6] == 'Z'
+                and self.bits[7] == 'Z'
+            )  # Special case 2
+            or (
+                self.bits[0] == '1'
+                and self.bits[1] == 'Z'
+                and self.bits[2] == 'Z'
+                and self.bits[6] == '0'
+                and self.bits[7] == 'Z'
+            )  # Special case 3
+            or (
+                self.bits[0] == '1'
+                and self.bits[1] == 'Z'
+                and self.bits[2] == '1'
+                and self.bits[6] == 'Z'
+                and self.bits[7] == 'Z'
+            )  # Special case 4
+        ):
             self.bits[7] = '1'
-            return({"bits": (self.bits[0], self.bits[1], self.bits[2], self.bits[6], self.bits[7]), "error": '0', "change": '1'})
-        elif self.bits[0] == 'Z' and self.bits[1] == '1' and self.bits[2] == '1' and self.bits[6] == 'Z' and self.bits[7] == 'Z':
-            # Special case 2
-            self.bits[7] = '1'
-            return({"bits": (self.bits[0], self.bits[1], self.bits[2], self.bits[6], self.bits[7]), "error": '0', "change": '1'})
-        elif self.bits[0] == '1' and self.bits[1] == 'Z' and self.bits[2] == 'Z' and self.bits[6] == '0' and self.bits[7] == 'Z':
-            # Special case 3
-            self.bits[7] = '1'
-            return({"bits": (self.bits[0], self.bits[1], self.bits[2], self.bits[6], self.bits[7]), "error": '0', "change": '1'})
-        elif self.bits[0] == '1' and self.bits[1] == 'Z' and self.bits[2] == '1' and self.bits[6] == 'Z' and self.bits[7] == 'Z':
-            # Special case 4
-            self.bits[7] = '1'
-            return({"bits": (self.bits[0], self.bits[1], self.bits[2], self.bits[6], self.bits[7]), "error": '0', "change": '1'})
+            return {
+                "bits": (
+                    self.bits[0],
+                    self.bits[1],
+                    self.bits[2],
+                    self.bits[6],
+                    self.bits[7],
+                ),
+                "error": '0',
+                "change": '1',
+            }
 
-        if change == '0':
+        self.change = change
+
+        if self.change == '0':
             self.old_bits = self.bits[0] + self.bits[1] + self.bits[2] + self.bits[6] + self.bits[7]
-        error = '0'
+        self.error = '0'
 
         # Half Adder 1
-        result = HalfAdder.HalfAdder([self.bits[0], self.bits[1], self.bits[3], self.bits[4]]).solve()
-        if (result["error"] == '0'):
-            if (result["change"] == '1'):
-                self.bits[0] = result["bits"][0]
-                self.bits[1] = result["bits"][1]
-                self.bits[3] = result["bits"][2]
-                self.bits[4] = result["bits"][3]
-                # print('HA1 Change:', self.bits)
-                change = '1'
-        else:
-            error = '1'
+        self._solve_half_adder(0, 1, 3, 4)
 
         # Half Adder 2
-        result = HalfAdder.HalfAdder([self.bits[2], self.bits[3], self.bits[6], self.bits[5]]).solve()
-        if (result["error"] == '0' and error == '0'):
-            if (result["change"] == '1'):
-                self.bits[2] = result["bits"][0]
-                self.bits[3] = result["bits"][1]
-                self.bits[6] = result["bits"][2]
-                self.bits[5] = result["bits"][3]
-                # print('HA2 Change:', self.bits)
-                return(self.solve('1'))
-        else:
-            error = '1'
+        self._solve_half_adder(2, 3, 6, 5)
 
         # OR
         result = gates.or_gate(self.bits[4], self.bits[5], self.bits[7])
-        if (result["error"] == '0' and error == '0'):
+        if (result["error"] == '0' and self.error == '0'):
             if (result["change"] == '1'):
                 self.bits[4] = result["gates"][0]
                 self.bits[5] = result["gates"][1]
                 self.bits[7] = result["gates"][2]
-                # print('OR Change:', self.bits)
                 return(self.solve('1'))
         else:
             self.bits = list("ZZZZZZZZ")
-            error = '1'
-            change = '1'
+            self.error = '1'
+            self.change = '1'
         
-        if error == '0':
-            change = '1' if self.old_bits != self.bits[0] + self.bits[1] + self.bits[2] + self.bits[6] + self.bits[7] else '0'
+        if self.error == '0':
+            self.change = '1' if self.old_bits != self.bits[0] + self.bits[1] + self.bits[2] + self.bits[6] + self.bits[7] else '0'
 
-        return({"bits": (self.bits[0], self.bits[1], self.bits[2], self.bits[6], self.bits[7]), "error": error, "change": change})
+        return({"bits": (self.bits[0], self.bits[1], self.bits[2], self.bits[6], self.bits[7]), "error": self.error, "change": self.change})
+    
+    def _solve_half_adder(self, a_index, b_index, sum_index, cout_index):
+        result = HalfAdder.HalfAdder([self.bits[a_index], self.bits[b_index], self.bits[sum_index], self.bits[cout_index]]).solve()
+        if (result["error"] == '0'):
+            if (result["change"] == '1'):
+                self.bits[a_index] = result["bits"][0]
+                self.bits[b_index] = result["bits"][1]
+                self.bits[sum_index] = result["bits"][2]
+                self.bits[cout_index] = result["bits"][3]
+                return self.solve('1')
+        else:
+            self.error = '1'
