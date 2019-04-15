9fa0cae4373fc6e18413659877c9608aa8e5d97b
Patrick Bollinger <owner@pjbollinger.com>
2019-04-15 08:12:24 -0400

    Reduce complexity of StandardRippleCarryArrayMultiplier

--- pyfactorization/RippleCarryArrayMultiplier.py
+++ pyfactorization/RippleCarryArrayMultiplier.py
@@ -82,113 +82,140 @@ class StandardRippleCarryArrayMultiplier(RippleCarryArrayMultiplier):
         #          ||└---- 2 input Sum In
         #          |└----- 1 inout Y
         #          └------ 0 inout X
-        error = '0'
+        self.change = change
+        self.error = '0'
+        array_solve_dict = {
+            ("i_eq_0", "j_eq_0"): self._solve_top_right_corner,
+            ("i_gt_0", "j_eq_0"): self._solve_first_column,
+            ("i_eq_last", "j_eq_0"): self._solve_first_column,
+            ("i_eq_0", "j_gt_0"): self._solve_top_row,
+            ("i_eq_0", "j_eq_last"): self._solve_top_row,
+            ("i_eq_last", "j_eq_last"): self._solve_bottom_left_corner,
+            ("i_eq_last", "j_gt_0"): self._solve_bottom_row,
+            ("i_gt_0", "j_eq_last"): self._solve_last_column,
+            ("i_gt_0", "j_gt_0"): self._solve_interior,
+        }
         for i in range(len(self.input_b)):
             for j in range(len(self.input_a)):
-                if (i == 0 and j == 0):
-                    # Top Right Corner
-                    result = MultiplierCell.MultiplierCell([self.input_a[j], self.input_b[i], '0', '0', self.output[i+j], self.carry[i][j]]).solve()
-                    if (result["error"] == '0' and self.output[i+j] == result["bits"][4]):
-                        if (result["change"] == '1'):
-                            self.input_a[j] = result["bits"][0]
-                            self.input_b[i] = result["bits"][1]
-                            self.carry[i][j] = result["bits"][5]
-                            change = '1'
-                    else:
-                        error = '1'
-                    self.sum[i][j] = None
-                elif (i > 0 and j == 0):
-                    # Firt Column
-                    result = MultiplierCell.MultiplierCell([self.input_a[j], self.input_b[i], self.sum[i-1][j+1], '0', self.output[i+j], self.carry[i][j]]).solve()
-                    if (result["error"] == '0' and self.output[i+j] == result["bits"][4]):
-                        if (result["change"] == '1'):
-                            self.input_a[j] = result["bits"][0]
-                            self.input_b[i] = result["bits"][1]
-                            self.sum[i-1][j+1] = result["bits"][2]
-                            self.carry[i][j] = result["bits"][5]
-                            change = '1'
-                    else:
-                        error = '1'
-                    self.sum[i][j] = None
-                elif (i == 0 and j > 0):
-                    # Top Row
-                    result = MultiplierCell.MultiplierCell([self.input_a[j], self.input_b[i], '0', self.carry[i][j-1], self.sum[i][j], self.carry[i][j]]).solve()
-                    if (result["error"] == '0'):
-                        if (result["change"] == '1'):
-                            self.input_a[j] = result["bits"][0]
-                            self.input_b[i] = result["bits"][1]
-                            self.carry[i][j-1] = result["bits"][3]
-                            self.sum[i][j] = result["bits"][4]
-                            self.carry[i][j] = result["bits"][5]
-                            change = '1'
-                    else:
-                        error = '1'
-                elif (i == len(self.input_b)-1 and j == len(self.input_a)-1):
-                    # Bottom Left Corner
-                    result = MultiplierCell.MultiplierCell([self.input_a[j], self.input_b[i], self.carry[i-1][j], self.carry[i][j-1], self.output[i+j], self.output[i+j+1]]).solve()
-                    if (result["error"] == '0' and self.output[i+j] == result["bits"][4] and self.output[i+j+1] == result["bits"][5]):
-                        if (result["change"] == '1'):
-                            self.input_a[j] = result["bits"][0]
-                            self.input_b[i] = result["bits"][1]
-                            self.carry[i-1][j] = result["bits"][2]
-                            self.carry[i][j-1] = result["bits"][3]
-                            change = '1'
-                    else:
-                        error = '1'
-                    self.sum[i][j] = None
-                    self.carry[i][j] = None
-                elif (i == len(self.input_a)-1):
-                    # Bottom Row
-                    result = MultiplierCell.MultiplierCell([self.input_a[j], self.input_b[i], self.sum[i-1][j+1], self.carry[i][j-1], self.output[i+j], self.carry[i][j]]).solve()
-                    if (result["error"] == '0' and self.output[i+j] == result["bits"][4]):
-                        if (result["change"] == '1'):
-                            self.input_a[j] = result["bits"][0]
-                            self.input_b[i] = result["bits"][1]
-                            self.sum[i-1][j+1] = result["bits"][2]
-                            self.carry[i][j-1] = result["bits"][3]
-                            self.carry[i][j] = result["bits"][5]
-                            change = '1'
-                    else:
-                        error = '1'
-                    self.sum[i][j] = None
-                elif (j == len(self.input_b)-1):
-                    # Last column
-                    result = MultiplierCell.MultiplierCell([self.input_a[j], self.input_b[i], self.carry[i-1][j], self.carry[i][j-1], self.sum[i][j], self.carry[i][j]]).solve()
-                    if (result["error"] == '0'):
-                        if (result["change"] == '1'):
-                            self.input_a[j] = result["bits"][0]
-                            self.input_b[i] = result["bits"][1]
-                            self.carry[i-1][j] = result["bits"][2]
-                            self.carry[i][j-1] = result["bits"][3]
-                            self.sum[i][j] = result["bits"][4]
-                            self.carry[i][j] = result["bits"][5]
-                            change = '1'
-                    else:
-                        error = '1'
-                else:
-                    # Interior
-                    result = MultiplierCell.MultiplierCell([self.input_a[j], self.input_b[i], self.sum[i-1][j+1], self.carry[i][j-1], self.sum[i][j], self.carry[i][j]]).solve()
-                    if (result["error"] == '0'):
-                        if (result["change"] == '1'):
-                            self.input_a[j] = result["bits"][0]
-                            self.input_b[i] = result["bits"][1]
-                            self.sum[i-1][j+1] = result["bits"][2]
-                            self.carry[i][j-1] = result["bits"][3]
-                            self.sum[i][j] = result["bits"][4]
-                            self.carry[i][j] = result["bits"][5]
-                            change = '1'
-                    else:
-                        error = '1'
-                if error == '1':
-                    break
-            if error == '1':
-                break
-        if error == '1':
-            print(error)
-            return({"input_a": ''.join(self.input_a)[::-1], "input_b": ''.join(self.input_b)[::-1], "error": error})
-        if change == '1':
+                i_key = self._index_i_to_key(i)
+                j_key = self._index_j_to_key(j)
+                array_solve_dict.get((i_key, j_key), self._solve_interior)(i, j)
+                if self.error == '1':
+                    print(self.error)
+                    return({"input_a": ''.join(self.input_a)[::-1], "input_b": ''.join(self.input_b)[::-1], "error": self.error})
+        if self.change == '1':
             return(self.solve())
-        return({"input_a": ''.join(self.input_a)[::-1], "input_b": ''.join(self.input_b)[::-1], "error": error})
+        return({"input_a": ''.join(self.input_a)[::-1], "input_b": ''.join(self.input_b)[::-1], "error": self.error})
+
+    def _solve_top_right_corner(self, i, j):
+        result = MultiplierCell.MultiplierCell([self.input_a[j], self.input_b[i], '0', '0', self.output[i+j], self.carry[i][j]]).solve()
+        if (result["error"] == '0' and self.output[i+j] == result["bits"][4]):
+            if (result["change"] == '1'):
+                self.input_a[j] = result["bits"][0]
+                self.input_b[i] = result["bits"][1]
+                self.carry[i][j] = result["bits"][5]
+                self.change = '1'
+        else:
+            self.error = '1'
+        self.sum[i][j] = None
+
+    def _solve_first_column(self, i, j):
+        result = MultiplierCell.MultiplierCell([self.input_a[j], self.input_b[i], self.sum[i-1][j+1], '0', self.output[i+j], self.carry[i][j]]).solve()
+        if (result["error"] == '0' and self.output[i+j] == result["bits"][4]):
+            if (result["change"] == '1'):
+                self.input_a[j] = result["bits"][0]
+                self.input_b[i] = result["bits"][1]
+                self.sum[i-1][j+1] = result["bits"][2]
+                self.carry[i][j] = result["bits"][5]
+                self.change = '1'
+        else:
+            self.error = '1'
+        self.sum[i][j] = None
+
+    def _solve_top_row(self, i, j):
+        result = MultiplierCell.MultiplierCell([self.input_a[j], self.input_b[i], '0', self.carry[i][j-1], self.sum[i][j], self.carry[i][j]]).solve()
+        if (result["error"] == '0'):
+            if (result["change"] == '1'):
+                self.input_a[j] = result["bits"][0]
+                self.input_b[i] = result["bits"][1]
+                self.carry[i][j-1] = result["bits"][3]
+                self.sum[i][j] = result["bits"][4]
+                self.carry[i][j] = result["bits"][5]
+                self.change = '1'
+        else:
+            self.error = '1'
+
+    def _solve_bottom_left_corner(self, i, j):
+        result = MultiplierCell.MultiplierCell([self.input_a[j], self.input_b[i], self.carry[i-1][j], self.carry[i][j-1], self.output[i+j], self.output[i+j+1]]).solve()
+        if (result["error"] == '0' and self.output[i+j] == result["bits"][4] and self.output[i+j+1] == result["bits"][5]):
+            if (result["change"] == '1'):
+                self.input_a[j] = result["bits"][0]
+                self.input_b[i] = result["bits"][1]
+                self.carry[i-1][j] = result["bits"][2]
+                self.carry[i][j-1] = result["bits"][3]
+                self.change = '1'
+        else:
+            self.error = '1'
+        self.sum[i][j] = None
+        self.carry[i][j] = None
+
+    def _solve_bottom_row(self, i, j):
+        result = MultiplierCell.MultiplierCell([self.input_a[j], self.input_b[i], self.sum[i-1][j+1], self.carry[i][j-1], self.output[i+j], self.carry[i][j]]).solve()
+        if (result["error"] == '0' and self.output[i+j] == result["bits"][4]):
+            if (result["change"] == '1'):
+                self.input_a[j] = result["bits"][0]
+                self.input_b[i] = result["bits"][1]
+                self.sum[i-1][j+1] = result["bits"][2]
+                self.carry[i][j-1] = result["bits"][3]
+                self.carry[i][j] = result["bits"][5]
+                self.change = '1'
+        else:
+            self.error = '1'
+        self.sum[i][j] = None
+
+    def _solve_last_column(self, i, j):
+        result = MultiplierCell.MultiplierCell([self.input_a[j], self.input_b[i], self.carry[i-1][j], self.carry[i][j-1], self.sum[i][j], self.carry[i][j]]).solve()
+        if (result["error"] == '0'):
+            if (result["change"] == '1'):
+                self.input_a[j] = result["bits"][0]
+                self.input_b[i] = result["bits"][1]
+                self.carry[i-1][j] = result["bits"][2]
+                self.carry[i][j-1] = result["bits"][3]
+                self.sum[i][j] = result["bits"][4]
+                self.carry[i][j] = result["bits"][5]
+                self.change = '1'
+        else:
+            self.error = '1'
+
+    def _solve_interior(self, i, j):
+        result = MultiplierCell.MultiplierCell([self.input_a[j], self.input_b[i], self.sum[i-1][j+1], self.carry[i][j-1], self.sum[i][j], self.carry[i][j]]).solve()
+        if (result["error"] == '0'):
+            if (result["change"] == '1'):
+                self.input_a[j] = result["bits"][0]
+                self.input_b[i] = result["bits"][1]
+                self.sum[i-1][j+1] = result["bits"][2]
+                self.carry[i][j-1] = result["bits"][3]
+                self.sum[i][j] = result["bits"][4]
+                self.carry[i][j] = result["bits"][5]
+                self.change = '1'
+        else:
+            self.error = '1'
+
+    def _index_i_to_key(self, i):
+        if i == 0:
+            return "i_eq_0"
+        elif i == len(self.input_b) - 1:
+            return "i_eq_last"
+        else:
+            return "i_gt_0"
+
+    def _index_j_to_key(self, j):
+        if j == 0:
+            return "j_eq_0"
+        elif j == len(self.input_a) - 1:
+            return "j_eq_last"
+        else:
+            return "j_gt_0"
 
 
 class OptimizedRippleCarryArrayMultiplier(RippleCarryArrayMultiplier):
