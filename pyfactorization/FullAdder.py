2cd89f0ba6add61ef254b2682734535aa3598cc1
Patrick Bollinger <owner@pjbollinger.com>
2019-04-16 19:20:31 -0400

    Fix critical complex logical expression

--- pyfactorization/FullAdder.py
+++ pyfactorization/FullAdder.py
@@ -15,55 +15,22 @@ class FullAdder:
         #          |└------ 1 input B
         #          └------- 0 input A
 
+    def _current_io(self):
+        return (self.bits[0], self.bits[1], self.bits[2], self.bits[6], self.bits[7])
+
     def solve(self, change='0'):
-        if (
-            (
-                self.bits[0] == 'Z'
-                and self.bits[1] == '1'
-                and self.bits[2] == 'Z'
-                and self.bits[6] == '0'
-                and self.bits[7] == 'Z'
-            )  # Special case 1
-            or (
-                self.bits[0] == 'Z'
-                and self.bits[1] == '1'
-                and self.bits[2] == '1'
-                and self.bits[6] == 'Z'
-                and self.bits[7] == 'Z'
-            )  # Special case 2
-            or (
-                self.bits[0] == '1'
-                and self.bits[1] == 'Z'
-                and self.bits[2] == 'Z'
-                and self.bits[6] == '0'
-                and self.bits[7] == 'Z'
-            )  # Special case 3
-            or (
-                self.bits[0] == '1'
-                and self.bits[1] == 'Z'
-                and self.bits[2] == '1'
-                and self.bits[6] == 'Z'
-                and self.bits[7] == 'Z'
-            )  # Special case 4
-        ):
+        self.change = change
+        if self.change == '0':
+            self.old_bits = "".join(self._current_io())
+        self.error = '0'
+        if (self.old_bits in ["Z1Z0Z", "Z11ZZ", "1ZZ0Z", "1Z1ZZ"]):
+            # Special cases
             self.bits[7] = '1'
             return {
-                "bits": (
-                    self.bits[0],
-                    self.bits[1],
-                    self.bits[2],
-                    self.bits[6],
-                    self.bits[7],
-                ),
+                "bits": self._current_io(),
                 "error": '0',
                 "change": '1',
             }
-
-        self.change = change
-
-        if self.change == '0':
-            self.old_bits = self.bits[0] + self.bits[1] + self.bits[2] + self.bits[6] + self.bits[7]
-        self.error = '0'
 
         # Half Adder 1
         self._solve_half_adder(0, 1, 3, 4)
@@ -85,9 +52,9 @@ class FullAdder:
             self.change = '1'
         
         if self.error == '0':
-            self.change = '1' if self.old_bits != self.bits[0] + self.bits[1] + self.bits[2] + self.bits[6] + self.bits[7] else '0'
+            self.change = '1' if self.old_bits != "".join(self._current_io()) else '0'
 
-        return({"bits": (self.bits[0], self.bits[1], self.bits[2], self.bits[6], self.bits[7]), "error": self.error, "change": self.change})
+        return({"bits": self._current_io(), "error": self.error, "change": self.change})
     
     def _solve_half_adder(self, a_index, b_index, sum_index, cout_index):
         result = HalfAdder.HalfAdder([self.bits[a_index], self.bits[b_index], self.bits[sum_index], self.bits[cout_index]]).solve()
